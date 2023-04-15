d0ab57ccd6f8531d2a92ff9b33eb62bd8e285f02
rcapelli <rociocapelli@gmail.com>
2023-04-15 13:21:52 -0300

    Se incluyen Objetos, Arrays y métodos de búsqueda y filtrado

---
+++ README.md
@@ -0,0 +1,38 @@
+
+## Project Title
+
+Sitio web de un cine
+
+
+## Authors
+
+Rocío Capelli
+
+
+## Documentation
+
+El presente incluye:
+* Archivo HTML y archivo JS, referenciado en el HTML por etiqueta <script src="js/main.js"></script>
+* Declaración y asiganción de variables.
+* Algoritmos con condicionales.
+* Algoritmos utilizando ciclos.
+* Funciones para realizar diversas operaciones.
+* Objetos de JS.
+* Arrays.
+* Métodos de búsqueda y filtrado sobre el Array.
+* Muestra mediante "alert" de los datos ingresados por el usuario.
+
+
+## Feedback
+
+Esta es la segunda pre-entrega del proyecto final que se encuentra en proceso de desarrollo.
+Se simula la compra de entradas para ir al cine.
+Primero, el usuario debe ingresar con su usuario y contraseña, en caso de no estar registrado le pide los datos para poder crear una cuenta.
+Segundo, procede a la selección de las distintas opciones como ser: cine, película, formato, idioma, día, hora, cantidad de entradas.
+Y por último, elige forma de pago y en base a eso calcula el total a abonar por las entradas.
+
+
+## Used By
+
+Este proyecto puede ser usado por los tutores y el profesor de la comisión 39505 para que puedan dar sus opiniones, sugerencias, correcciones y así poder avanzar con el proyecto final.
+
---
+++ js/main.js
@@ -0,0 +1,232 @@
+/* declaración de funciones */
+
+function registroCuenta() {
+    nombreUsuario = prompt("El correo ingresado no se encuentra registrado. Por favor, ingrese su nombre.");
+    usuarioNuevo = prompt("Ingrese su e-mail para poder registrarse.");
+    usuario = usuarioNuevo.toUpperCase();
+    contrasenaNueva = prompt("Ingrese su contraseña.");
+    contrasena = contrasenaNueva;
+    do {
+        usuarioVerificacion = prompt("Ingrese su e-mail para acceder a su cuenta.");
+        contrasenaverificacion = prompt("Ingrese su contraseña.");
+    } while (!(usuarioVerificacion.toUpperCase() == usuario && contrasenaverificacion == contrasena));
+    alert("¡Le damos la bienvenida a Cinema Paradiso!");
+}
+
+function seleccionCine() {
+    let valorObtenido = eleccionCine.toUpperCase();
+    while (!(cines.some((unCine) => unCine.id == valorObtenido))) {
+        eleccionCine = prompt("No seleccionó ninguna opción. Vuelva a ingresar la opción del complejo al que desea ir: \n A. Belgrano \n B. Palermo \n C. Recoleta");
+        valorObtenido = eleccionCine.toUpperCase();
+    }
+    if (cines.some((unCine) => unCine.id == valorObtenido)) {
+        let indice = cines.map((unCine) => unCine.id).indexOf(valorObtenido);
+        return eleccionCine = cines[indice].nombre;
+    }
+}
+
+function seleccionPelicula() {
+    let valorObtenido = eleccionPelicula.toUpperCase();
+    while (!(peliculas.some((unaPelicula) => unaPelicula.id == valorObtenido))) {
+        eleccionPelicula = prompt("No seleccionó ninguna opción. Vuelva a ingresar la opción de la película que quiere ir a ver: \n A. El Padrino \n B. El Rey León \n C. Forrest Gump \n D. Jurassic Park \n E. La vida de bella \n F. Los siete pecados capitales \n G. Mi pobre angelito \n H. Quién quiere ser millonario? \n I. Rocky \n J. Volver al futuro");
+        valorObtenido = eleccionPelicula.toUpperCase();
+    }
+    if (peliculas.some((unaPelicula) => unaPelicula.id == valorObtenido)) {
+        let indice = peliculas.map((unaPelicula) => unaPelicula.id).indexOf(valorObtenido);
+        return eleccionPelicula = peliculas[indice].nombre;
+    }
+}
+
+function seleccionFormato() {
+    let valorObtenido = eleccionFormato.toUpperCase();
+    while (!(formatos.some((unFormato) => unFormato.id == valorObtenido))) {
+        eleccionFormato = prompt("No seleccionó ninguna opción. Vuelva a ingresar la opción del formato que prefiera: \n A. 2D \n B. 3D \n C. 4D");
+        valorObtenido = eleccionFormato.toUpperCase();
+    }
+    if (formatos.some((unFormato) => unFormato.id == valorObtenido)) {
+        let indice = formatos.map((unFormato) => unFormato.id).indexOf(valorObtenido);
+        return eleccionFormato = formatos[indice].nombre;
+    }
+}
+
+function seleccionIdioma() {
+    let valorObtenido = eleccionIdioma.toUpperCase();
+    while (!(idiomas.some((unIdioma) => unIdioma.id == valorObtenido))) {
+        eleccionIdioma = prompt("No seleccionó ninguna opción. Vuelva a ingresar la opción para el idioma: \n A. Doblada \n B. Subtitulada");
+        valorObtenido = eleccionIdioma.toUpperCase();
+    }
+    if (idiomas.some((unIdioma) => unIdioma.id == valorObtenido)) {
+        let indice = idiomas.map((unIdioma) => unIdioma.id).indexOf(valorObtenido);
+        return eleccionIdioma = idiomas[indice].nombre;
+    }
+}
+
+function seleccionDia() {
+    let valorObtenido = eleccionDia.toUpperCase();
+    while (!(dias.some((unDia) => unDia.id == valorObtenido))) {
+        eleccionDia = prompt("No seleccionó ninguna opción. Vuelva a ingresar la opción del día que desea ir: \n A. LUN 06 de febrero \n B. MAR 07 de febrero \n C. MIE 08 de febrero \n D. JUE 09 de febrero \n E. VIE 10 de febrero \n F. SAB 11 de febrero \n G. DOM 12 de febrero");
+        valorObtenido = eleccionDia.toUpperCase();
+    }
+    if (dias.some((unDia) => unDia.id == valorObtenido)) {
+        let indice = dias.map((unDia) => unDia.id).indexOf(valorObtenido);
+        return eleccionDia = dias[indice].nombre;
+    }
+}
+
+function seleccionHora() {
+    let valorObtenido = eleccionHora.toUpperCase();
+    while (!(horas.some((unaHora) => unaHora.id == valorObtenido))) {
+        eleccionHora = prompt("No seleccionó ninguna opción. Vuelva a ingresar la opción de la hora que prefiere: \n A. 18 hs \n B. 20 hs \n C. 22 hs");
+        valorObtenido = eleccionHora.toUpperCase();
+    }
+    if (horas.some((unaHora) => unaHora.id == valorObtenido)) {
+        let indice = horas.map((unaHora) => unaHora.id).indexOf(valorObtenido);
+        return eleccionHora = horas[indice].nombre;
+    }
+}
+
+function seleccionCantidad() {
+    let valorObtenido = eleccionCantidad;
+    let resultado = cantidades.find(entradas => entradas == valorObtenido);
+    while (valorObtenido != resultado) {
+        eleccionCantidad = prompt("No seleccionó ninguna opción. Vuelva a ingresar la cantidad de entradas que desea adquirir. Recuerde que puede comprar hasta 6 entradas.");
+        valorObtenido = eleccionCantidad;
+        resultado = cantidades.find(entradas => entradas == valorObtenido);
+    }
+    if (valorObtenido == resultado) {
+        return eleccionCantidad = valorObtenido;
+    }
+}
+
+function seleccionMedioPago(){
+    while (medioPago.toUpperCase() != "SI" && medioPago.toUpperCase() != "NO") {
+        medioPago = prompt("La respuesta proporcionada no es válida. Para acceder al beneficio del 50% de descuento debe pagar con Banco Santander. Desea abonar por este medio? (SI / NO)");
+    }
+    if (medioPago.toUpperCase() == "SI") {
+        medioPago = "Banco Santander";
+        importe = eleccionCantidad * 1500 / 2
+    }
+    if (medioPago.toUpperCase() == "NO") {
+        medioPago = "Sin promoción";
+        importe = eleccionCantidad * 1500
+    }
+}
+
+
+
+/* declaración de variables globales, invocación de funciones */
+
+let usuario = "";
+let contrasena;
+let nombreUsuario;
+let usuarioVerificacion;
+let contrasenaverificacion;
+let importe;
+
+let cines = [
+    {id: "A", nombre: "Belgrano",},
+    {id: "B", nombre: "Palermo",},
+    {id: "C", nombre: "Recoleta",}
+];
+
+let peliculas = [
+    {id: "A", nombre: "El Padrino",},
+    {id: "B", nombre: "El Rey León",},
+    {id: "C", nombre: "Forrest Gump",},
+    {id: "D", nombre: "Jurassic Park",},
+    {id: "E", nombre: "La vida de bella",},
+    {id: "F", nombre: "Los siete pecados capitales",},
+    {id: "G", nombre: "Mi pobre angelito",},
+    {id: "H", nombre: "Quién quiere ser millonario?",},
+    {id: "I", nombre: "Rocky",},
+    {id: "J", nombre: "Volver al futuro",}
+];
+
+let formatos = [
+    {id: "A", nombre: "2D",},
+    {id: "B", nombre: "3D",},
+    {id: "C", nombre: "4D",}
+];
+
+let idiomas = [
+    {id: "A", nombre: "Doblada",},
+    {id: "B", nombre: "Subtitulada",}
+];
+
+let dias = [
+    {id: "A", nombre: "LUN 06 de febrero",},
+    {id: "B", nombre: "MAR 07 de febrero",},
+    {id: "C", nombre: "MIE 08 de febrero",},
+    {id: "D", nombre: "JUE 09 de febrero",},
+    {id: "E", nombre: "VIE 10 de febrero",},
+    {id: "F", nombre: "SAB 11 de febrero",},
+    {id: "G", nombre: "DOM 12 de febrero",}
+];
+
+let horas = [
+    {id: "A", nombre: "18 hs",},
+    {id: "B", nombre: "20 hs",},
+    {id: "C", nombre: "22 hs",}
+];
+
+let cantidades = [1, 2, 3, 4, 5, 6];
+
+
+
+let usuarioNuevo = prompt("Ingrese su e-mail para acceder a su cuenta.");
+let contrasenaNueva = prompt("Ingrese su contraseña.");
+
+if (usuarioNuevo.toUpperCase() == usuario && contrasenaNueva == contrasena) {
+    alert("¡Le damos la bienvenida a Cinema Paradiso!");
+}
+
+if (usuarioNuevo.toUpperCase() == usuario && contrasenaNueva != contrasena) {
+    while (contrasenaNueva != contrasena) {
+        contrasenaNueva = prompt("La contraseña no es correcta. Vuelva a ingresarla.");
+    }
+    alert("¡Le damos la bienvenida a Cinema Paradiso!");
+}
+
+if (usuarioNuevo.toUpperCase() != usuario) {
+    registroCuenta();
+}
+
+let eleccionCine = prompt("Ingrese la opción del complejo al que desea ir: \n A. Belgrano \n B. Palermo \n C. Recoleta");
+
+seleccionCine();
+
+let eleccionPelicula = prompt("Ingrese la opción de la película que quiere ir a ver: \n A. El Padrino \n B. El Rey León \n C. Forrest Gump \n D. Jurassic Park \n E. La vida de bella \n F. Los siete pecados capitales \n G. Mi pobre angelito \n H. Quién quiere ser millonario? \n I. Rocky \n J. Volver al futuro");
+
+seleccionPelicula();
+
+let eleccionFormato =prompt("Ingrese la opción del formato que prefiera: \n A. 2D \n B. 3D \n C. 4D");
+
+seleccionFormato();
+
+let eleccionIdioma = prompt("Ingrese la opción para el idioma: \n A. Doblada \n B. Subtitulada");
+
+seleccionIdioma();
+
+let eleccionDia = prompt("Ingrese la opción del día que desea ir: \n A. LUN 06 de febrero \n B. MAR 07 de febrero \n C. MIE 08 de febrero \n D. JUE 09 de febrero \n E. VIE 10 de febrero \n F. SAB 11 de febrero \n G. DOM 12 de febrero");
+
+seleccionDia();
+
+let eleccionHora = prompt("Ingrese la opción de la hora que prefiere: \n A. 18 hs \n B. 20 hs \n C. 22 hs");
+
+seleccionHora();
+
+let eleccionCantidad = prompt("Ingrese la cantidad de entradas que desea adquirir. Recuerde que puede comprar hasta 6 entradas.");
+
+seleccionCantidad();
+
+let medioPago = prompt("El precio de cada entrada es de $1500. Puede acceder al beneficio del 50% de descuento pagando con Banco Santander. Desea abonar por este medio? (SI / NO)");
+
+seleccionMedioPago();
+
+
+
+/* muestra del detalle de la compra a través de un alert */
+
+alert("DETALLE DE LA COMPRA: \nCine: " + eleccionCine + "\nPelícula: " + eleccionPelicula + "\nFormato: " + eleccionFormato + "\nIdioma: " + eleccionIdioma + "\nDía: " + eleccionDia + "\nHora: " + eleccionHora + "\nCantidad: " + eleccionCantidad + " entrada/s" + "\nPrecio total a pagar: $ " + importe);
+
+
